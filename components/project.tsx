ab0c0494e70e1cc4100a1a28722a0f0586d41778
NueltCodes <olaniranemmanuelt@gmail.com>
2023-08-27 01:18:19 +0100

    Created the the theme mode which enables users switch from dark to light theme, also made the project component fully mobile responsive. Modified the rest of the component with new features like dark theme classes.

--- components/project.tsx
+++ components/project.tsx
@@ -24,13 +24,14 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
+      <section className="bg-slate-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem] overflow-y-scroll scrollbar-hide">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+          <ul className="flex flex-wrap mt-4 gap-2  ">
+            <p className="font-semibold">Used Tech stack</p>
             {tags.map((tag, index) => (
               <li
                 className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
@@ -46,18 +47,18 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
-          className="absolute h-full hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+          className="sm:absolute p-3 sm:p-0 object-contain h-full w-full  sm:block top-8 -right-40 sm:w-[28.25rem] rounded-t-lg shadow-2xl
             transition 
-            group-hover:scale-[1.04]
-            group-hover:-translate-x-3
-            group-hover:translate-y-3
-            group-hover:-rotate-2
+            sm:group-hover:scale-[1.04]
+            sm:group-hover:-translate-x-3
+            sm:group-hover:translate-y-3
+            sm:group-hover:-rotate-2
     
-            group-even:group-hover:translate-x-3
-            group-even:group-hover:translate-y-3
-            group-even:group-hover:rotate-2
+            sm:group-even:group-hover:translate-x-3
+            sm:group-even:group-hover:translate-y-3
+            sm:group-even:group-hover:rotate-2
     
-            group-even:right-[initial] group-even:-left-40"
+            sm:group-even:right-[initial] sm:group-even:-left-40"
         />
       </section>
     </motion.div>
--- components/projects.tsx
+++ components/projects.tsx
@@ -6,19 +6,19 @@ import { projectsData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
 
-const Projects = () => {
-  const { ref } = useSectionInView("Projects", 0.5);
+export default function Projects() {
+  const { ref } = useSectionInView("Projects", 0.3);
 
   return (
-    <section ref={ref} id="projects" className="scroll-mt-28">
-      <SectionHeading>My Start-up project</SectionHeading>
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+      <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
-          <Project key={index} {...project} />
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
         ))}
       </div>
     </section>
   );
-};
-
-export default Projects;
+}
