9052eb7fa728e32cee76402649b738af27eff0ae
NueltCodes <olaniranemmanuelt@gmail.com>
2023-09-02 22:37:39 +0100

    Added my correct Cv and Meta certificate pdf

--- components/intro.tsx
+++ components/intro.tsx
@@ -80,7 +80,7 @@ const Intro = () => {
         <div className="flex justify-center mb-3">
           <a
             className="group max-w-[450px] justify-center bg-white px-3 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-            href="/CV.pdf"
+            href="/Meta.pdf"
             download
           >
             Certificate from Meta Mobile course{" "}
@@ -112,7 +112,7 @@ const Intro = () => {
 
           <a
             className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-            href="https://www.linkedin.com/in/tobi-emmanuel-113392224/"
+            href="https://www.linkedin.com/in/emmanuet/"
             target="_blank"
           >
             <BsLinkedin />
